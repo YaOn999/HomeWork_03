b1fda9484aba050fd264f7d427be29fc30d35f8c
YaOn999 <lumous999@gmail.com>
2022-12-08 22:39:28 +0300

    add HW_01

---
+++ HM_01/Program.cs
@@ -0,0 +1,36 @@
+﻿// Напишите программу, которая принимает на вход 
+// пятизначное число и проверяет, является ли оно 
+// палиндромом.
+
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+string EnterNumber(string text)
+{
+    Console.Write(text);
+    string number = Console.ReadLine();
+    return number;
+}
+
+string PalindDef(string n)
+{
+    if (n.Length == 5)
+    {
+        if (n[0] == n[4] && n[1] == n[3]) return "это палиндром";
+        else
+        {
+            return "это не палиндром";
+        }
+    }
+    else return "Вы ввели не пятизначное число";
+}
+
+// 1. ввод данных
+string number = EnterNumber("введите пятизначное число для проверки на палиндром: ");
+
+// 2. анализ данных
+string palind = PalindDef(number);
+
+// 3. вывод информации
+Console.WriteLine(palind);
