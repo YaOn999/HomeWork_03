c2a26aa281f7190b999c9372c99ff2ed196dba8d
YaOn999 <lumous999@gmail.com>
2022-12-08 22:44:30 +0300

    add HW_03

---
+++ HW_03/Program.cs
@@ -0,0 +1,30 @@
+﻿// Напишите программу, которая принимает на вход число (N) 
+// и выдаёт таблицу кубов чисел от 1 до N.
+
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+void FindNumbers(int number)
+{
+    double result = 0;
+    int n = 1;
+    while (n <= number)
+    {
+        result = Math.Pow(n, 3);
+        Console.Write($"{result}, ");
+        n++;
+    }
+        }
+
+int EnterData(string text)
+{
+    Console.Write(text);
+    int number = int.Parse(Console.ReadLine());
+    return number;
+}
+
+// 2. запрос числа
+int number = EnterData("Введите число: ");
+
+// 3. вывод информации
+FindNumbers(number);
